b047f6721714bab5d5e75fe0583b6b3e35aebd44
sty4786237 <ys6616@nyu.edu>
2025-07-29 13:33:37 +0800

    Show Image

--- project-images/sketch.js
+++ project-images/sketch.js
@@ -1,8 +1,13 @@
+let img;
+
 function setup() {
   let canvas = createCanvas(800, 500);
   canvas.parent("p5-canvas-container");
+  //not ideal
+  img = loadImage("assets/colorful.jpg");
 }
 
 function draw() {
   background(220);
+  image(img, 0, 0);
 }
