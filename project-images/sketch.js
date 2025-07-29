bab9ce5c69ab5b792a81277bd1569fb6da026b3b
sty4786237 <ys6616@nyu.edu>
2025-07-29 14:09:04 +0800

    Emoji test

--- project-images/sketch.js
+++ project-images/sketch.js
@@ -1,29 +1,29 @@
 let img;
 
+
 function preload() {
-  //img=loadImage("filepath");
-  img=loadImage("assets/colorful.jpg");
-  //loadSound()
-  //loadString()
-  //loadFont()
-  //loadJSON()....
+ img = loadImage("assets/emoji.png");
 }
+
 
 function setup() {
-  let canvas = createCanvas(500, 281);
-  canvas.parent("p5-canvas-container");
-  //not ideal
-  img = loadImage("assets/colorful.jpg");
+ let canvas = createCanvas(500, 281);
+ canvas.parent("p5-canvas-container");
+
+
+ background(100);
 }
 
+
 function draw() {
-  background(220);
+ // background(100);
 
-  let r=map(mouseX, 0, width, 0, 255);
-  let g=map(mouseY, 0, height, 0, 255);
-  let b=map(mouseX, 0, width, 255, 0);
 
-  tint(r, g, b);
-  image(img, 0, 0);
-  //image(img, x, y, (width), (height));
+ let x = random(width);
+ let y = random(height);
+ let size = random(10, 50);
+
+
+ imageMode(CENTER);
+ image(img, x, y, size, size); //(img, x, y, (w), (h));
 }
