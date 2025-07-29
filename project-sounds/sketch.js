d4ab92bd1850ee0d3874d605c06e72843995356c
sty4786237 <ys6616@nyu.edu>
2025-07-29 15:31:53 +0800

    Soundplay2

--- project-sounds/sketch.js
+++ project-sounds/sketch.js
@@ -1,23 +1,40 @@
-let snd;
+let sound;
+
 
 function preload() {
-  snd = loadSound("assets/kick.mp3");
+ sound = loadSound("assets/song-long.mp3");
 }
+
+
 function setup() {
-  let canvas = createCanvas(800, 500);
-  canvas.parent("p5-canvas-container");
+ let canvas = createCanvas(800, 500);
+ canvas.parent("p5-canvas-container");
 }
+
 
 function draw() {
-  background(220);
-  let volValue = map(mouseY, 0, height, 1.0, 0.0,true);
-  snd.setVolume(volValue);
-  let panValue = map(mouseX, 0, width, -1.0, 1.0, true);
-  snd.pan(panValue);
-  //center:0, left:-1, right:1(100%)
-  snd.rate(map(mouseX, 0, width, 0, 2.0, true)); //100%,% value
+ background(220);
+
+
+ let volValue = map(mouseY, 0, height, 1.0, 0.0, true);
+ //sound.setVolume(volValue);
+
+
+ let panValue = map(mouseX, 0, width, -1.0, 1.0, true);
+ //sound.pan(panValue); // center: 0, left: -1, right: 100%
+
+
+ let rateValue = map(mouseY, 0, height, 2.0, 0.01, true);
+ //sound.rate(rateValue); // % value
 }
 
+
 function mousePressed() {
-  snd.play();
+ if (sound.isPlaying()) {
+   //sound.pause();
+   sound.stop(); // start over
+ } else {
+   //sound.play();
+   sound.loop();
+ }
 }
