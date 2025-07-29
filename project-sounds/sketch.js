6b123bbea1c8ab37f5182061b238d5c9c7811a7a
sty4786237 <ys6616@nyu.edu>
2025-07-29 15:12:37 +0800

    Soundplay1

--- project-sounds/sketch.js
+++ project-sounds/sketch.js
@@ -10,6 +10,12 @@ function setup() {
 
 function draw() {
   background(220);
+  let volValue = map(mouseY, 0, height, 1.0, 0.0,true);
+  snd.setVolume(volValue);
+  let panValue = map(mouseX, 0, width, -1.0, 1.0, true);
+  snd.pan(panValue);
+  //center:0, left:-1, right:1(100%)
+  snd.rate(map(mouseX, 0, width, 0, 2.0, true)); //100%,% value
 }
 
 function mousePressed() {
