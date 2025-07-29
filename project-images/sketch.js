34d7be3544e1e6b5cb9f12b71c55888d08777428
sty4786237 <ys6616@nyu.edu>
2025-07-29 14:02:27 +0800

    Colorful Image Test

--- project-images/sketch.js
+++ project-images/sketch.js
@@ -1,7 +1,16 @@
 let img;
 
+function preload() {
+  //img=loadImage("filepath");
+  img=loadImage("assets/colorful.jpg");
+  //loadSound()
+  //loadString()
+  //loadFont()
+  //loadJSON()....
+}
+
 function setup() {
-  let canvas = createCanvas(800, 500);
+  let canvas = createCanvas(500, 281);
   canvas.parent("p5-canvas-container");
   //not ideal
   img = loadImage("assets/colorful.jpg");
@@ -9,5 +18,12 @@ function setup() {
 
 function draw() {
   background(220);
+
+  let r=map(mouseX, 0, width, 0, 255);
+  let g=map(mouseY, 0, height, 0, 255);
+  let b=map(mouseX, 0, width, 255, 0);
+
+  tint(r, g, b);
   image(img, 0, 0);
+  //image(img, x, y, (width), (height));
 }
