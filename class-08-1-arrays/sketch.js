4a84b8a33dfe462d61d8c9766977a27812c0c4c4
sty4786237 <ys6616@nyu.edu>
2025-07-31 14:19:18 +0800

    Colors

--- class-08-1-arrays/sketch.js
+++ class-08-1-arrays/sketch.js
@@ -1,36 +1,28 @@
-let arr=[1,3,5,10,100,"green","cyan"];
+let colors = [
+ "Aquamarine",
+ "Chartreuse",
+ "CornflowerBlue",
+ "Cornsilk",
+ "DarkOrchid",
+ "DarkTurquoise",
+ "DeepPink"
+]; // array!
+
 
 function setup() {
  let canvas = createCanvas(800, 500);
  canvas.parent("p5-canvas-container");
-
 }
 
 
 function draw() {
-    background(220);
+ background(220);
 
-    textSize(30);
-    //first index is 0
-    text(arr[0],100,50)
 
-    arr[0] = 1000;
-    text(arr[0],100,100)
-
-    //add a new number to the end of the array
-    arr.push(1000);
-    text(arr[7],100,150);
-
-    let result=arr[1]+arr[2];
-    text(result,100,200);
-
-    let lastIndex=arr.length-1;
-    text(arr[lastIndex],100,250);
-
-    //loop through the array
-    for (let i=0; i<arr.length; i++) {
-        text(arr[i],500,100+i*30);
-    }
-
-    noLoop();
+ for (let i = 0; i < colors.length; i++) {
+   let x = 100;
+   let y = 100 + i * 60;
+   textSize(40);
+   text(colors[i], x, y);
+ }
 }
