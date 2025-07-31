1769846f61e018a6e4d2f5b3578fbe723f86d75e
sty4786237 <ys6616@nyu.edu>
2025-07-31 14:16:44 +0800

    ArrayTest

--- class-08-1-arrays/sketch.js
+++ class-08-1-arrays/sketch.js
@@ -1,29 +1,36 @@
-let x, y, xSpeed, ySpeed, dia;
+let arr=[1,3,5,10,100,"green","cyan"];
 
 function setup() {
-    
-    let canvas = createCanvas(800, 500);
-    canvas.parent("p5-canvas-container");
+ let canvas = createCanvas(800, 500);
+ canvas.parent("p5-canvas-container");
 
-    x = width / 2;
-    y = height / 2;
-    xSpeed = random(-2, 2);
-    ySpeed = random(-2, 2);
-    dia = random(10,30);
 }
 
 
 function draw() {
     background(220);
-    x+= xSpeed;
-    y+= ySpeed;
-    //bounce
-    if (x<0 || x > width) {
-        xSpeed *= -1;
+
+    textSize(30);
+    //first index is 0
+    text(arr[0],100,50)
+
+    arr[0] = 1000;
+    text(arr[0],100,100)
+
+    //add a new number to the end of the array
+    arr.push(1000);
+    text(arr[7],100,150);
+
+    let result=arr[1]+arr[2];
+    text(result,100,200);
+
+    let lastIndex=arr.length-1;
+    text(arr[lastIndex],100,250);
+
+    //loop through the array
+    for (let i=0; i<arr.length; i++) {
+        text(arr[i],500,100+i*30);
     }
-    if (y<0 || y > height) {
-        ySpeed *= -1;
-    }
-    //draw circle
-    circle(x, y, dia);
+
+    noLoop();
 }
