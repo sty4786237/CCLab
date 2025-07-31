8d940e40ac2329263ad0bcd7f85d8b772dc714c6
sty4786237 <ys6616@nyu.edu>
2025-07-31 16:11:17 +0800

    ObjectDancerExample

--- class-08-1-arrays/sketch.js
+++ class-08-1-arrays/sketch.js
@@ -1,67 +1,48 @@
-let balls = [];
+let dancer;
 
 
 function setup() {
  let canvas = createCanvas(800, 500);
  canvas.parent("p5-canvas-container");
- background(0);
 
 
- for (let i = 0; i < 1000; i++) {
-   balls[i] = new Ball();
- }
-
-
+ dancer = new MoonDancer();
+ //console.log(dancer.x, dancer.dia);
 }
 
 
 function draw() {
-   background(0);
+ background(100);
 
 
- for (let i = 0; i < balls.length; i++) {
-   let eachBall = balls[i];
-   eachBall.move();
-   eachBall.bounce();
-   eachBall.display();
+ if (mouseIsPressed) {
+   dancer.move();
  }
+ dancer.display();
 }
 
 
-//
 
 
-class Ball {
+// CLASS: Blueprint, plan, design
+class MoonDancer {
+ // very special function
  constructor() {
-   // variables --> properties
-   this.x = random(width); //width / 2;
+   // variables --> Properties
+   this.x = width / 2;
    this.y = height / 2;
-   this.dia = random(10, 30);
-   this.xSpeed = 0; //random(-3, 3);
-   this.ySpeed = random(-3, 3);
+   this.dia = 100;
    //
-   this.r = random(255);
-   this.g = random(255);
-   this.b = random(255);
+   this.xSpeed = random(-3, 3);
  }
- // functions --> methods! actions
+ // functions --> methods
  move() {
    this.x += this.xSpeed;
-   this.y += this.ySpeed;
- }
- bounce() {
-   if (this.x < 0 || this.x > width) {
-     this.xSpeed *= -1;
-   }
-   if (this.y < 0 || this.y > height) {
-     this.ySpeed *= -1;
-   }
  }
  display() {
-   noStroke();
-   fill(this.r, this.g, this.b);
    circle(this.x, this.y, this.dia);
  }
 }
+
 
 
